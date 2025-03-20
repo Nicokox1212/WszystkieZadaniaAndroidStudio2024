348df4cd15a784dab22057e5295fb611ace12291
Nicokox1212 <144319801+Nicokox1212@users.noreply.github.com>
2025-03-20 09:40:02 +0100

    korutyny

---
+++ korutyny/app/src/main/java/com/example/korutyny/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.korutyny
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.korutyny.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        var time : Long = System.currentTimeMillis()
+        var sekunda = 0
+        lifecycleScope.launch {
+            while (true) {
+                binding.textView1.text = sekunda.toString()
+                delay(1000)
+                sekunda += 1
+            }
+        }
+        lifecycleScope.launch {
+            while (true) {
+            val time2: Long = System.currentTimeMillis()
+            binding.textView2.text = (time2 - time).toString()
+            delay(10)
+            }
+
+        }
+        binding.button1.setOnClickListener{
+            sekunda = 0
+            binding.textView1.text = sekunda.toString()
+    }
+        binding.button2.setOnClickListener{
+            time = System.currentTimeMillis()
+        }
+        
+    }
+}
